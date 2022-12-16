8a33b5184699555c57b34a97a64ff5ae642954d6
DonUggioni <renan.uggioni@gmail.com>
2022-12-16 16:28:24 +0000

    Removed unused hooks

--- src/components/expenseList/ExpenseList.js
+++ src/components/expenseList/ExpenseList.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import { ListValuesContext } from "../store/AppContext";
 
 import ExpenseListItem from "../expenseListItem/ExpenseListItem";
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,14 +1,14 @@
-import React, { useContext, useEffect } from 'react';
-import { ListValuesContext } from '../components/store/AppContext';
-import BarChartComponent from '../components/barChart/BarChartComponent';
-import Budget from '../components/budget/Budget';
-import ExpenseList from '../components/expenseList/ExpenseList';
-import Expenses from '../components/expenses/Expenses';
-import Header from '../components/header/Header';
-import UserInputs from '../components/inputs/UserInputs';
-import Remaining from '../components/remaining/Remaining';
+import React, { useContext } from "react";
+import { ListValuesContext } from "../components/store/AppContext";
+import BarChartComponent from "../components/barChart/BarChartComponent";
+import Budget from "../components/budget/Budget";
+import ExpenseList from "../components/expenseList/ExpenseList";
+import Expenses from "../components/expenses/Expenses";
+import Header from "../components/header/Header";
+import UserInputs from "../components/inputs/UserInputs";
+import Remaining from "../components/remaining/Remaining";
 
-import './MainPage.scss';
+import "./MainPage.scss";
 
 function MainPage() {
   const appContext = useContext(ListValuesContext);
@@ -31,12 +31,12 @@ function MainPage() {
     }
   }
   return (
-    <div className="main__wrapper">
+    <div className='main__wrapper'>
       <Header />
-      <main className="main">
-        <div className="user_interactions_wrapper">
-          <div className="user_interactions_wrapper--inner_wrapper">
-            <div className="balance_values">{displayValues()}</div>
+      <main className='main'>
+        <div className='user_interactions_wrapper'>
+          <div className='user_interactions_wrapper--inner_wrapper'>
+            <div className='balance_values'>{displayValues()}</div>
             <UserInputs />
           </div>
           <BarChartComponent />
